0b56bafc0134489453a1e3dfb450b3edc554e081
mariarayane <mariarayanesilva@hotmail.com>
2019-11-04 23:51:03 -0200

    atividadegit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static int somar(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
